1f5be5d650b3a64f608d53da1883737e8023539c
elprofedotti <elprofedotti@gmail.com>
2023-02-23 12:41:02 -0300

    ok

--- src/ar/fusion/Main.java
+++ src/ar/fusion/Main.java
@@ -1,5 +1,20 @@
+package ar.fusion;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double precio;
+        System.out.println("Ingrese el precio:");
+        Scanner scanner=new Scanner(System.in);
+        precio=scanner.nextDouble();
+        double precioMasIVA=damePrecioMasIVA(precio);
+
+        System.out.println("Usted ingreso: "+precio);
+        System.out.println("Precio con IVA(21%) --> "+precioMasIVA);
+    }
+    static double damePrecioMasIVA(double precio){
+        //IVA = 21%
+        return precio*=1.21;
     }
 }
